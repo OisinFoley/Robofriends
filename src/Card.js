f2bbf97f43ad897f682e5bafd992cc3e459e0281
oisfo <oisfo@BE351ZVP>
2019-06-14 17:49:34 +0200

    Initial commit, testing tachyons was imported correctly. Removed redundant default js and style files

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/meow?200x200" alt="robots"/>
+      <div>
+        <h2>wub wub</h2>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
