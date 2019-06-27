793768491b1ea7ba70f7bf32841aec337d4ba375
oisinfoley <oisinfoley@yahoo.co.uk>
2019-06-27 23:17:54 +0200

    Updates

---
+++ precache-manifest.861157ead03b032e0d879ea05eb21c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68a1d47cab46be2491c086e41245391",
+    "url": "/OisinFoley/Robofriends/index.html"
+  },
+  {
+    "revision": "370824843c732dab2a1a",
+    "url": "/OisinFoley/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b80987ed9d80169861b5",
+    "url": "/OisinFoley/Robofriends/static/css/main.2f2f74a3.chunk.css"
+  },
+  {
+    "revision": "370824843c732dab2a1a",
+    "url": "/OisinFoley/Robofriends/static/js/2.3eee5a13.chunk.js"
+  },
+  {
+    "revision": "b80987ed9d80169861b5",
+    "url": "/OisinFoley/Robofriends/static/js/main.a91c1ce1.chunk.js"
+  },
+  {
+    "revision": "06199daffad645cf90b6",
+    "url": "/OisinFoley/Robofriends/static/js/runtime~main.5c1a7623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/OisinFoley/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
